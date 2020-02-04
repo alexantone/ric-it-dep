3e801ede37f15f79eca64a28237e8e1a72d4c464
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2020-02-04 15:17:18 -0500

    Use aarch64 images
    - hard set REC K8s endpoint;
    - extend uninstall to also remove stale secret;
    
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>
    Signed-off-by: Alexandru Antone <Alexandru.Antone@enea.com>

--- _install/ric-uninstall.sh
+++ _install/ric-uninstall.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 helm list | grep -o -P 'r1-[^\ \t]*' | xargs helm del --purge
+# FIXME: alav: previous installations might leave behind leftover secrets
+# which break tiller-secret-generator reinstallation with:
+# Error from server (AlreadyExists): secrets "secret-helm-client-ricxapp" already exists
+# + kubectl label secret secret-helm-client-ricxapp 'app=helm' 'name=secret-helm-client-ricxapp
+# 'app' already has a value (helm), and --overwrite is false
+# 'name' already has a value (secret-helm-client-ricxapp), and --overwrite is false
+
+
+kubectl delete secrets -n ricinfra secret-helm-client-ricxapp secret-tiller-ricxapp
 
